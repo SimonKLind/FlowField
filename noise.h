202348487b681092733d3d4937bf0ba25627d707
zabotio <simon9817@gmail.com>
2017-03-07 22:00:55 +0100

    Update noise.h

--- noise.h
+++ noise.h
@@ -1,13 +1,21 @@
+/** This header is my own variation of Perlin noise,
+  * with some minor changes from Perlin's original algorithm
+  * Specifically: 
+  * A set number of randomly generated gradient vectors, 
+  * which get chosen based on integer part of x, y and z passed
+  * A single hash to determine which of the random vectors are picked
+  * instead of 8 different hashes */
+
 #ifndef NOISE_H
 #define NOISE_H
 
 #include <random>
 // #include <iostream>
 
-#define NUM_VECTORS 256
+#define NUM_VECTORS 256 // Number of randomly generated gradient vectors
 
 class NoiseGenerator{
-	struct Vector{
+	struct Vector{ // A little vector struct to simplify things
 		double x;
 		double y;
 		double z;
@@ -18,21 +26,17 @@ class NoiseGenerator{
 	int octaves = 8;
 	double persistence = 0.5;
 
+	/** Linear interpolate */
 	double lerp(double a, double b, double x){
 		return a+x*(b-a);
 	}
 
+	/** Same fade as Perlin's implementation */
 	double fade(double x){
 		return x*x*x*(x*(x*6-15)+10);
 	}
 
-	/*double grad(double x, double y, double z){
-		double a = (double)rand()/rand.max()*2-1;
-		double b = (double)rand()/rand.max()*2-1;
-		double c = (double)rand()/rand.max()*2-1;
-		return a*x+b*y+c*z;
-	}*/
-
+	/** Multiplies x, y and z with vector corresponding to hash */
 	double grad(int hash, double x, double y, double z){
 		// int index = hash&0xFF;
 		int index = hash%NUM_VECTORS;
@@ -40,10 +44,10 @@ class NoiseGenerator{
 	}
 
 public:
-
+	/** Seeded constructor */
 	NoiseGenerator(int seed){
 		rand.seed(seed);
-		for(int i=0; i<NUM_VECTORS; ++i){
+		for(int i=0; i<NUM_VECTORS; ++i){ // Generates random vectors
 			g[i].x = ((double)rand()/rand.max())*2-1;
 			g[i].y = ((double)rand()/rand.max())*2-1;
 			g[i].z = ((double)rand()/rand.max())*2-1;
@@ -51,6 +55,9 @@ public:
 		}
 	}
 
+	/** The perlin noise function itself
+	  * doesn't differ much from Perlin's original.
+	  * Only real difference is the hash */
 	double perlin(double x, double y = 0, double z = 0){
 		int hash = 7 + (int)x;
 		hash = hash*31 + (int)y;
@@ -72,7 +79,8 @@ public:
 		y2 = lerp(x1, x2, v);
 		return (lerp(y1, y2, w)+1)/2;
 	}
-
+	
+	/** Using operator() to call noise with octaves */
 	double operator()(double x, double y = 0, double z = 0){
 		double total = 0, amp = 1, freq = 1, maxVal = 0;
 		for(int i=0; i<octaves; ++i){
